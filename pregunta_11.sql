e0dd2605ab461c83169cb963f74e622af4f66fcc
AnaGT <amgiraldot@unal.edu.co>
2022-04-16 17:12:27 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -37,4 +37,4 @@
 --  0  2018     6
 --
 --  >>> Escriba su codigo a partir de este punto <<<
-    SELECT count(*) FROM tbl1 WHERE strftime('%Y', c14) = '2018';
+    SELECT COUNT(*) FROM tbl1 WHERE strftime('%Y', c14) = '2018';
